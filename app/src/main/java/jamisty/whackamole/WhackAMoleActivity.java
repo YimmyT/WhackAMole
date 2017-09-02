c15c2ca8c46bced93ba8dcd64aa0444d9e134f00
YimmyT <holidaych1881@yahoo.com>
2017-09-02 18:56:25 -0500

    Initial commit

---
+++ app/src/main/java/jamisty/whackamole/WhackAMoleActivity.java
@@ -0,0 +1,13 @@
+package jamisty.whackamole;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhackAMoleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.whackamole_layout);
+    }
+}

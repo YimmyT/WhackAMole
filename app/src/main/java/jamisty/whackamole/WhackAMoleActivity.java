1f7e48fea48338b7b051ed2d7a730f3b53056ae3
YimmyT <holidaych1881@yahoo.com>
2017-09-02 21:40:09 -0500

    set title screen to launch view/layout
    removed ConstraintLayout, added LinearLayout.
    Added view to layout.
    default activity to use layout, force fullscreen

--- app/src/main/java/jamisty/whackamole/WhackAMoleActivity.java
+++ app/src/main/java/jamisty/whackamole/WhackAMoleActivity.java
@@ -2,12 +2,21 @@ package jamisty.whackamole;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 
 public class WhackAMoleActivity extends Activity {
+
+    private WhackAMoleView myWhackAMoleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.whackamole_layout);
+        myWhackAMoleView = (WhackAMoleView)findViewById(R.id.mole);
+        myWhackAMoleView.setKeepScreenOn(true);
     }
 }
